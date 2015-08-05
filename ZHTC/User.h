5f37307ce021d4aff0f09dc9be5972adc41d25e2
keith-kriri <694343197@qq.com>
2015-08-05 16:05:55 +0800

    加个Model

---
+++ ZHTC/User.h
@@ -0,0 +1,17 @@
+//
+//  User.h
+//  ZHTC
+//
+//  Created by RWX－MAC on 15/8/5.
+//  Copyright (c) 2015年 RWX－MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic,copy)NSString *name;
+
+@property (nonatomic,copy)NSString *age;
+
+@end
---
+++ ZHTC/User.m
@@ -0,0 +1,16 @@
+
+//
+//  User.m
+//  ZHTC
+//
+//  Created by RWX－MAC on 15/8/5.
+//  Copyright (c) 2015年 RWX－MAC. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+
+@end
+
